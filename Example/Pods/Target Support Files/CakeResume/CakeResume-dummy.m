2b51588dc475469e8fc0380528177efb787ea356
Miguel <gannasong@gmail.com>
2020-06-08 09:57:01 +0800

    test create pod

---
+++ Example/Pods/Target Support Files/CakeResume/CakeResume-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CakeResume : NSObject
+@end
+@implementation PodsDummy_CakeResume
+@end
---
+++ Example/Pods/Target Support Files/Pods-CakeResume_Tests/Pods-CakeResume_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CakeResume_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CakeResume_Tests
+@end
